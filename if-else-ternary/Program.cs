93d62c57ae846c9eabda2f6b0817af96a4bec61a
Can Çetin <108129872+cancetn@users.noreply.github.com>
2023-02-20 23:24:28 +0300

    Add files via upload

---
+++ if-else-ternary/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace if_else_ternary;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+if (time>6 && time<11)
+    Console.WriteLine("Günaydın!");
+else if (time<=18)
+    Console.WriteLine("İyi günler!");
+else
+    Console.WriteLine("İyi geceler!");
+
+string sonuc = time<=18 ? "İyi Günler!" : "İyi Geceler";
+Console.WriteLine(sonuc);
+
+sonuc = time>=6 && time<11 ? "Günaydın!": time <= 18 ? "İyi günler" : "İyi geceler!";
+Console.WriteLine(sonuc);
+    }
+}
