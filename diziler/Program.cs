72818953ac8a10c65786695d5e07c24851667a28
Can Çetin <108129872+cancetn@users.noreply.github.com>
2023-02-21 13:02:09 +0300

    Add files via upload

---
+++ diziler/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace diziler;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Dizi Tanımlama
+
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //Dizilere değer atama ve erişim 
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+
+            // Döngüler ve dizi kullanımı
+            // Klavyeden girilen n tane sayının ortalamasını alan bir örnek
+
+        Console.Write("Kac adet sayi gireceksiniz ? : ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] sayilar = new int[n];
+
+        float toplam = 0;
+
+        for(int i = 0; i<n; i++){
+
+            Console.Write("Sayiyi giriniz : ");
+
+            sayilar[i] = Convert.ToInt32(Console.ReadLine());
+
+            toplam = toplam + sayilar[i];
+
+        }
+
+        Console.WriteLine("Ortalama : " + (float)(toplam/n));
+
+        Console.WriteLine("Girilen sayilar listesi :");
+
+        foreach(var sayi in sayilar){
+            Console.WriteLine(sayi);
+        }
+
+    }
+}
